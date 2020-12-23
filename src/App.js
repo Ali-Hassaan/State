1c83e537f2a9461fc509b9bdfd181e074ca4714f
Ali Hassaan <alihassaan0310@gmail.com>
2020-12-23 20:12:33 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Value from './Value';
+import increment from './img/add.png';
+import decrement from './img/minus.png';
+import shift from './img/switch.png'
 import './App.css';
 
 function App() {
+  let [Number, setNumber]= useState(0)
+  let [Time, setTime] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`night ${Time ? 'day' : ''}`}>
+      <h3>Good {Time ? 'Morning' : 'Night'}</h3>
+      <Value val={Number} />
+      <br />
+      <button onClick={() => setNumber(++Number)}><img src={increment} alt='increment' height='70' width='70' /></button>
+      <br />
+      <br />
+      <button onClick={() => setNumber(--Number)}><img src={decrement} alt='decrement' height='70' width='70' /></button>
+      <br />
+      <br />
+      <button onClick={() => setTime(!Time)}><img src={shift} alt='shift' height='70' width='70' /></button>
+    
     </div>
   );
 }
---
+++ src/Value.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Value (props) {
+    return (
+        <div>
+            <h1>Value: {props.val} </h1>
+        </div>
+    )
+}
+
+export default Value;
